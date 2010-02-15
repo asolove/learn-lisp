d3def8f71e67292a3abc8b074534fe839a0f347f
asolove <asolove@gmail.com>
2010-02-15 10:43:24 -0500

    Finished single patterns

--- paip/pattern-matcher.lisp
+++ paip/pattern-matcher.lisp
@@ -51,7 +51,55 @@
 
 ;; Single (general) pattern
 (defun single-pattern-p (pattern)
-  nil)
+  (and (consp pattern) 
+       (variable-pattern-p (first pattern))
+       (single-pattern-fn (first pattern))))
+
+(defun single-pattern-fn (name)
+  (get name 'single-pattern))
+
+(defun single-pattern-match (pattern input bindings)
+  (funcall (single-pattern-fn (first pattern))
+	   (rest pattern) input bindings))
+
+(setf (get '?is 'single-pattern) 'match-is)
+
+(defun match-is (pattern input bindings)
+  "(?x oddp) '3' no-bindings => '((?x . 3))"
+  (let* ((var (first pattern))
+	 (f (second pattern))
+	 (new-bindings (variable-pattern-match var input bindings)))
+    (or (and new-bindings (funcall f input) new-bindings)
+	fail)))
+
+(setf (get '?or 'single-pattern) 'match-or)
+
+(defun match-or (patterns input bindings)
+  "(?x ?y) '2 '((?x . 4)) => '((?y . 2) (?x . 4))"
+  (cond ((null patterns) fail)
+	((pat-match (car patterns) input bindings))
+	(t (match-or (cdr patterns) input bindings))))
+
+(setf (get '?and 'single-pattern) 'match-and)
+
+(defun match-and (patterns input bindings)
+  "(?x ?y) '2 '((?x . 2) (?y . 2)) => succeeds"
+  (if (null patterns)
+      bindings
+      (let ((new-bindings (pat-match (car patterns) input bindings)))
+	(if new-bindings
+	    (match-and (cdr patterns) input new-bindings)
+	    fail))))
+
+(setf (get '?not 'single-pattern) 'match-not)
+
+(defun match-not (patterns input bindings)
+  (cond ((null patterns) bindings)
+	((pat-match (car patterns) input bindings) fail)
+	(t (match-not (cdr patterns) input bindings))))
+  
+	  
+  
 
 ;; Multiple (general) pattern 
 (defun multiple-pattern-p (pattern)
